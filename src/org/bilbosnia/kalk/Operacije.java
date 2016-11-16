90f19616cd33ea3b40fd2f3e32eafbffc2946daf
Ivana Cupic <ivanamd82@gmail.com>
2016-11-16 12:58:48 +0100

    inicijalizacija

---
+++ src/org/bilbosnia/kalk/Operacije.java
@@ -0,0 +1,30 @@
+package org.bilbosnia.kalk;
+
+public class Operacije {
+	
+	public static void sabiranje () {
+		
+	}
+	
+	public static void oduzimanje () {
+		
+	}
+	
+	public static void mnozenje () {
+		
+	}
+	
+	public static void djeljenje () {
+		
+	}
+	
+	public static void korjenovanje () {
+		
+	}
+	
+	public static void kvadriranje () {
+	
+	}
+
+
+}
