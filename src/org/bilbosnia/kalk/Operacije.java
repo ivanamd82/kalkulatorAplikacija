b23bb20b3ef6245ea395f6232413213239a2a8c6
Mensur Hadzic <hadzic.mensur@hotmail.com>
2016-11-16 13:21:14 +0100

    zavrseno mnozenje i djeljenje

--- src/org/bilbosnia/kalk/Operacije.java
+++ src/org/bilbosnia/kalk/Operacije.java
@@ -15,11 +15,14 @@ public class Operacije {
 	}
 	
 	public static void mnozenje (double a, double b) {
-		
+		System.out.println(a * b);
 	}
 	
 	public static void djeljenje (double a, double b) {
-		
+		if (b == 0)
+			System.out.println("Greska (djeljenje nulom)!");
+		else
+			System.out.println(a / b);
 	}
 	
 	public static void korjenovanje (double a) {
